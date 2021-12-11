c7be98bbcfd2c4da227df2e5c3b7afaf56e9060f
ali.ozen <ali.ozen@metunic.com.tr>
2021-12-11 19:33:38 +0300

    redirect problemi giderildi #7

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -21,7 +21,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', Rules\Password::min(6)],
         ]);
