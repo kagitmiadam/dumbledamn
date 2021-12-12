76301cd49982adf593a5d2e06063e547d5a2c774
ali.ozen <ali.ozen@metunic.com.tr>
2021-12-12 19:32:49 +0300

    auth check yapildi #11

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Artisan;
+use Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Artisan::call("period:check");
+        if(Auth::check()) {
+            Artisan::call("period:check");
+        }
     }
 }
