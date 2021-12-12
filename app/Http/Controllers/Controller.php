2082b2ee443dd984656d5d7154d62e4bc864ece8
ali.ozen <ali.ozen@metunic.com.tr>
2021-12-12 20:26:22 +0300

    period cup fix #11

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+use App\Models\School;
+use App\Models\PeriodCup;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct() {
+        $active_school_id = School::where('status', 1)->pluck('id');
+        $period_cups = PeriodCup::whereIn('status', [0, 1])->whereIn('school_id', $active_school_id)->get();
+        if(!empty($period_cups->all())) {
+            \Artisan::call("period:check");
+        }
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Artisan;
-use Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(Auth::check()) {
-            Artisan::call("period:check");
-        }
+
     }
 }
